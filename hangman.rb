689c9c905a34903c2e8a914103e2d9cbcae8d91c
hansnery <hansnery@gmail.com>
2020-08-18 23:33:48 -0300

    made methods private

--- hangman.rb
+++ hangman.rb
@@ -2,29 +2,44 @@ class Hangman
   require "yaml"
   require_relative 'word.rb'
 
-  @hanged = false
-  @errors = 0
-  @hangman = ""
-  @displayed_characters = []
-  @typed_letters = []
-  
-  def self.create_secret_word
-    @word = Word.new.sample.chomp.upcase
-    number_of_characters = @word.chomp.length
-    number_of_characters.times do
-      @displayed_characters << "_"
+  @@hanged = false
+  @@errors = 0
+  @@hangman = ""
+  @@displayed_characters = []
+  @@typed_letters = []
+
+  def begin_game
+    create_secret_word
+    # puts "#{@word}"
+    puts "\nWelcome to Hangman!"
+    draw_hangman
+    display_characters
+    while @@hanged == false
+      ask_for_letter
+      check_matches
+      display_characters
     end
   end
 
-  def self.display_characters
-    puts @displayed_characters.join(" ")
-    puts "\nGuesses: #{@typed_letters.join(" ")}" unless @typed_letters.empty?
+  private
+  
+  def create_secret_word
+    @word = Word.new.sample.chomp.upcase
+    number_of_characters = @word.chomp.length
+    number_of_characters.times do
+      @@displayed_characters << "_"
+    end
   end
 
-  def self.draw_hangman
-    case @errors
+  def display_characters
+    puts @@displayed_characters.join(" ")
+    puts "\nGuesses: #{@@typed_letters.join(" ")}" unless @@typed_letters.empty?
+  end
+
+  def draw_hangman
+    case @@errors
     when 0
-      @hangman = %{
+      @@hangman = %{
         ------ 
              |
              |
@@ -32,7 +47,7 @@ class Hangman
              |
      }
     when 1
-      @hangman = %{
+      @@hangman = %{
         ------ 
         O    |
              |
@@ -40,7 +55,7 @@ class Hangman
              |
      }
     when 2
-      @hangman = %{
+      @@hangman = %{
         ------ 
         O    |
        /     |
@@ -48,7 +63,7 @@ class Hangman
              |
      }
     when 3
-      @hangman = %{
+      @@hangman = %{
         ------ 
         O    |
        /|    |
@@ -56,7 +71,7 @@ class Hangman
              |
      }
     when 4
-      @hangman = %{
+      @@hangman = %{
         ------ 
         O    |
        /|\\   |
@@ -64,7 +79,7 @@ class Hangman
              |
      }
     when 5
-      @hangman = %{
+      @@hangman = %{
         ------ 
         O    |
        /|\\   |
@@ -72,7 +87,7 @@ class Hangman
              |
      }
     when 6
-      @hangman = %{
+      @@hangman = %{
          ------ 
          O    |
         /|\\   |
@@ -80,33 +95,33 @@ class Hangman
               |
       }
     end
-    puts "#{@hangman}"
+    puts "#{@@hangman}"
   end
 
-  def self.check_matches
+  def check_matches
     @word.each_char.with_index do |letter, idx|
       if letter == @typed_letter
-        @displayed_characters[idx] = @typed_letter
+        @@displayed_characters[idx] = @typed_letter
       end
     end
-    if @displayed_characters.none? { |n| n == @typed_letter }
-      @errors += 1
+    if @@displayed_characters.none? { |n| n == @typed_letter }
+      @@errors += 1
       draw_hangman
     end
-    if @errors > 5
-      @hanged = true
+    if @@errors > 5
+      @@hanged = true
       ask_for_a_rematch
     end
   end
 
-  def self.ask_for_a_rematch
+  def ask_for_a_rematch
     puts "\nYou were hanged!\nTry again? Type yes or no."
     answer = gets.chomp.downcase
     if answer == "yes"
-      @hanged = false
-      @errors = 0
-      @displayed_characters = []
-      @typed_letters = []
+      @@hanged = false
+      @@errors = 0
+      @@displayed_characters = []
+      @@typed_letters = []
       begin_game
     elsif answer == "no"
       return
@@ -116,31 +131,13 @@ class Hangman
     end
   end
 
-  def self.ask_for_letter
+  def ask_for_letter
     puts "\nType a letter to figure out the secret word: "
     @typed_letter = gets.chomp.upcase
-    @typed_letters << @typed_letter
+    @@typed_letters << @typed_letter
   end
-
-  def self.begin_game
-    create_secret_word
-    # puts "#{@word}"
-    puts "\nWelcome to Hangman!"
-    draw_hangman
-    display_characters
-    while @hanged == false
-      ask_for_letter
-      check_matches
-      display_characters
-    end
-  end
-
-  begin_game
 end
 
 test = Hangman.new
-
+test.begin_game
 # serialized_object = YAML::dump(test)
-
-# test.begin_game
-# puts test
