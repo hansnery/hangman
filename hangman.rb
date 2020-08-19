f082377806b733458a66128b0d60c93beaccb1fb
hansnery <hansnery@gmail.com>
2020-08-18 21:18:01 -0300

    working code

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,8 @@ class Hangman
   require_relative 'word.rb'
 
   @hanged = false
+  @errors = 0
+  @hangman = ""
   
   def self.create_secret_word
     @word = Word.new.sample.chomp.upcase
@@ -14,6 +16,69 @@ class Hangman
 
   def self.display_characters
     puts @displayed_characters.join(" ")
+    puts "\n"
+  end
+
+  def self.draw_hangman
+    case @errors
+    when 0
+      @hangman = %{
+        ------ 
+             |
+             |
+             |
+             |
+     }
+    when 1
+      @hangman = %{
+        ------ 
+        O    |
+             |
+             |
+             |
+     }
+    when 2
+      @hangman = %{
+        ------ 
+        O    |
+       /     |
+             |
+             |
+     }
+    when 3
+      @hangman = %{
+        ------ 
+        O    |
+       /|    |
+             |
+             |
+     }
+    when 4
+      @hangman = %{
+        ------ 
+        O    |
+       /|\\   |
+             |
+             |
+     }
+    when 5
+      @hangman = %{
+        ------ 
+        O    |
+       /|\\   |
+       /     |
+             |
+     }
+    when 6
+      @hangman = %{
+         ------ 
+         O    |
+        /|\\   |
+        / \\   |
+              |
+      }
+    end
+    puts "#{@hangman}"
   end
 
   def self.check_matches
@@ -22,9 +87,28 @@ class Hangman
         @displayed_characters[idx] = @typed_letter
       end
     end
-
-    if @displayed_characters.none? { |n| n == "_" }
+    if @displayed_characters.none? { |n| n == @typed_letter }
+      @errors += 1
+      draw_hangman
+    end
+    if @errors > 5
       @hanged = true
+      ask_for_a_rematch
+    end
+  end
+
+  def self.ask_for_a_rematch
+    puts "\nYou were hanged!\nTry again? Type yes or no."
+    answer = gets.chomp.downcase
+    if answer == "yes"
+      @hanged = false
+      @errors = 0
+      begin_game
+    elsif answer == "no"
+      return
+    else
+      puts "You typed an invalid answer!"
+      ask_for_a_rematch
     end
   end
 
@@ -33,21 +117,17 @@ class Hangman
     @typed_letter = gets.chomp.upcase
   end
 
-  create_secret_word
-
-  puts "Welcome to Hangman! #{@word}"
-
-  display_characters
-
-  while @hanged == false
-    ask_for_letter
-
-    check_matches
-
+  def self.begin_game
+    create_secret_word
+    puts "\nWelcome to Hangman! #{@word}"
+    draw_hangman
     display_characters
+    while @hanged == false
+      ask_for_letter
+      check_matches
+      display_characters
+    end
   end
-  
-  #if there are matches, reveal them
-  
-  #if there are no matches draw another stick
+
+  begin_game
 end
